1270f338ec4bef650d63015e4cf99f0a8b53a5f4
Andrei Bezmen <andryshabez26@gmail.com>
2020-03-22 18:15:29 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let separate = [];
+    let decode = [];
+    let result = '';
+    for (let i = 0; i < (expr.length / 10); i++) {
+        separate[i] = expr.slice(i * 10, (i + 1) * 10);
+        decode[i] = '';
+        for (let j = 0; j < 10; j += 2) {
+            if (separate[i].slice(j, j + 2) === '10') {
+            decode[i] += '.';
+            } else if (separate[i].slice(j, j + 2) === '11') {
+            decode[i] += '-';
+            } else if (separate[i].slice(j, j + 2) === '**') {
+            j += 8
+            }
+        }
+    }
+  
+  
+    for (let i = 0; i < decode.length; i++) {
+        if (decode[i] === '') {
+        result += ' '
+        } else {
+        result += MORSE_TABLE[decode[i]]
+        }
+    }
+    return result
 }
 
 module.exports = {
